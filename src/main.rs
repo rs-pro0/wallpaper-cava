4f521f0ee0525ad7c4ccff98b70921420a50b3ef
rs-pro0 <rostyslav.yabchenko@gmail.com>
2024-06-14 02:37:19 +0300

    Moved shaders to seperate files

--- src/main.rs
+++ src/main.rs
@@ -93,36 +93,9 @@ fn array_from_config_color(color: ConfigColor) -> [f32; 4] {
     }
 }
 
-const VERTEX_SHADER_SRC: &str = r#"
-#version 430 core
-in vec2 position;
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-}
-"#;
+const VERTEX_SHADER_SRC: &str = include_str!("shaders/vertex_shader.glsl");
 
-const FRAGMENT_SHADER_SRC: &str = r#"
-#version 430 core
-layout(std430, binding = 0) buffer GradientColors {
-    int gradient_colors_size;
-    vec4 gradient_colors[];
-};
-uniform vec2 WindowSize;
-out vec4 fragColor;
-void main() {
-    if (gradient_colors_size == 1){
-        fragColor = gradient_colors[0];
-    } else{
-        float findex = (gl_FragCoord.y * float(gradient_colors_size - 1)) / WindowSize.y;
-        int index = int(findex);
-        float step=findex - float(index);
-        if (index == gradient_colors_size - 1){
-            index--;
-        }
-        fragColor = mix(gradient_colors[index], gradient_colors[index+1], step);
-    }
-}
-"#;
+const FRAGMENT_SHADER_SRC: &str = include_str!("shaders/fragment_shader.glsl");
 
 fn main() {
     let config_str = fs::read_to_string("config.toml").expect("Unable to read config file");
---
+++ src/shaders/fragment_shader.glsl
@@ -0,0 +1,20 @@
+#version 430 core
+layout(std430, binding = 0) buffer GradientColors {
+    int gradient_colors_size;
+    vec4 gradient_colors[];
+};
+uniform vec2 WindowSize;
+out vec4 fragColor;
+void main() {
+    if (gradient_colors_size == 1){
+        fragColor = gradient_colors[0];
+    } else{
+        float findex = (gl_FragCoord.y * float(gradient_colors_size - 1)) / WindowSize.y;
+        int index = int(findex);
+        float step=findex - float(index);
+        if (index == gradient_colors_size - 1){
+            index--;
+        }
+        fragColor = mix(gradient_colors[index], gradient_colors[index+1], step);
+    }
+}
